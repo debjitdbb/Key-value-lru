58dd24a5100d43b9ef801f29833f3e2767bebac8
debjit.datta <debjit.datta@artivatic.ai>
2020-12-30 17:14:30 +0530

    init commit

--- LRU.py
+++ LRU.py
@@ -1,12 +1,12 @@
 from collections import OrderedDict
-
+import json
 
 class LRUCache:
 
 
-    def __init__(self, capacity):
+    def __init__(self):
         self.cache = OrderedDict()
-        self.capacity = capacity
+        # self.capacity = capacity
 
 
     def get(self, key):
@@ -21,10 +21,13 @@ class LRUCache:
         if key in self.cache:
             print("The entered key in already present....")
             return
+        elif self.cache.__sizeof__() > 1e9:
+            self.cache.popitem(last = False)
+
         self.cache[key] = value 
         self.cache.move_to_end(key)
-        if len(self.cache.cache) > self.capacity:
-            self.cache.popitem(last = False)
+        # if len(self.cache.cache) > self.capacity:
+        #     self.cache.popitem(last = False)
         return   
 
     
@@ -34,3 +37,38 @@ class LRUCache:
             return
         self.cache.pop(key)
         return  
+
+temp = LRUCache()
+
+from flask import Flask,jsonify
+app = Flask(__name__)
+
+@app.route('/create', methods = ['PUT'])
+def create():
+    val = {'dsdfgs':'uhjdsgfkjsgd'}
+    key = '1'
+    temp.put(key,val)
+    # print(temp.cache)
+    with open('file.json', 'w') as f:
+        dictionaries = [temp.cache]
+        f.write(json.dumps(dictionaries))
+    with open('file.json','r') as read_file:
+        loaded_dict = json.loads(read_file.read())
+        print(loaded_dict[0])
+    # for i in temp:
+    #     print(i)
+    # return json.dumps(temp)
+    return loaded_dict[0], 200
+
+@app.route('/read',methods = ['PUT'])
+def read():
+    key = '1'
+    val = temp.get(key)
+    print(val)
+    return jsonify(temp.cache), 200
+
+@app.route('/delete', methods = ['PUT'])
+def erase():
+    key = 1
+    temp.remove(key)
+    return jsonify(temp.cache), 200
